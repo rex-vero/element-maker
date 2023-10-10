abf52829c0de60b09d2d4420b9933eb51c3fdcb1
rex-vero <142157808+rex-vero@users.noreply.github.com>
2023-10-10 16:13:10 +0330

    JS

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const eColor = document.getElementById('eColor');
 const tColor = document.getElementById('tColor');
 const height = document.getElementById('height');
 const width = document.getElementById('width');
+const font = document.getElementById('font');
 const btn = document.getElementById('btn');
 const maker = document.getElementById('maker');
 const rBtn = document.getElementById('rbtn');
@@ -12,31 +13,30 @@ const card = document.getElementById('card');
 btn.addEventListener('click', (e) => {
     e.preventDefault();
     form.style.display = 'none';
-    rBtn.style.display = 'block';
-    const newElement = document.createElement(element.value);
-    switch (element.value) {
-        case 'a':
-            const link = document.createElement('a');
-            link.setAttribute('href', '#')
-            link.innerText = `${text.value}`
-            link.style.backgroundColor = `${eColor.value}`
-            link.style.color = `${tColor.value}`
-            link.style.height = `${height.value}px`
-            link.style.width = `${width.value}px`
-            maker.appendChild(link)
-            break;
-
-        default:
-            newElement.innerText = `${text.value}`
-            break;
+    card.style.display = 'flex';
+    if (element.value === 'a') {
+        const link = document.createElement('a');
+        link.setAttribute('href', '#')
+        link.innerText = `${text.value}`
+        link.classList.add('center');
+        link.style.backgroundColor = `${eColor.value}`
+        link.style.color = `${tColor.value}`
+        link.style.fontSize = `${font.value}px`
+        link.style.height = `${height.value}px`
+        link.style.width = `${width.value}px`
+        maker.appendChild(link)
+    } else {
+        const newElement = document.createElement(element.value);
+        newElement.innerText = `${text.value}`
+        newElement.classList.add('center');
+        newElement.style.backgroundColor = `${eColor.value}`
+        newElement.style.color = `${tColor.value}`
+        newElement.style.fontSize = `${font.value}px`
+        newElement.style.height = `${height.value}px`
+        newElement.style.width = `${width.value}px`
+        maker.appendChild(newElement)
     }
-    newElement.style.backgroundColor = `${eColor.value}`
-    newElement.style.color = `${tColor.value}`
-    newElement.style.height = `${height.value}px`
-    newElement.style.width = `${width.value}px`
-    maker.appendChild(newElement)
 })
 rBtn.addEventListener('click', () => {
-        location.reload();
+    location.reload();
 })
-
